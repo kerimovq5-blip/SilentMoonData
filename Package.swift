5d9244b571789c211c592835793983e4878707d4
Qehreman <kerimovq5@gmail.com>
2026-08-15 15:03:40 +0400

    Data Package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SilentMoonData",
+    platforms: [
+        .iOS(.v16)]
+    ,
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SilentMoonData",
+            targets: ["SilentMoonData"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kerimovq5-blip/SilentMoonNetwork", branch: "master"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SilentMoonData" ,
+            dependencies: [
+                .product(name: "SilentMoonNetwork", package: "SilentMoonNetwork")
+            ]
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Sources/SilentMoonData/SilentMoonDTOs/ErrorModel.swift
@@ -0,0 +1,34 @@
+//
+//  ErrorModel.swift
+//  SilentMoonData
+//
+//  Created by Kerimov Qehreman on 15.08.26.
+//
+
+public struct ErrorModel : Decodable  ,Error {
+   private(set) var statuscode: Int?
+    let statusmessage: String?
+    let success: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case statuscode = "status_code"
+        case statusmessage = "status_message"
+        case success = "success"
+
+    }
+    mutating func setStatusCode(statusCode: Int) {
+        if self.statuscode == nil {
+            self.statuscode = statusCode
+        }
+    }
+    var localizedDescription: String {
+        if let statusmessage = statusmessage {
+            return statusmessage
+        }else if let statuscode = statuscode {
+           return  "Error with status code : \(statuscode)"
+
+        }
+        return " Unknown Error"
+
+    }
+}
