39d95faee24390cd292c8b1d53d2046269ee4f30
Qehreman <kerimovq5@gmail.com>
2026-08-22 21:58:42 +0400

    Sendable

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
                 ),
         .package(
             url: "https://github.com/kerimovq5-blip/SilentMoonDomain" ,
-            revision: "ece58890e81b5256688a4be9f69c0c817f4fda18"
+            revision: "11ac3bab78ed7551482ab46f6463af6d18c21892"
         )
     ],
     targets: [
