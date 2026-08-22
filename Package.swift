6b5645ff182f88c9f348cda5933e1c04865938bd
Qehreman <kerimovq5@gmail.com>
2026-08-22 18:41:45 +0400

    newVersion

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -34,5 +34,5 @@ let package = Package(
         ),
 
     ],
-    swiftLanguageModes: [.v6]
+    //swiftLanguageModes: [.v6]
 )
--- Sources/SilentMoonData/SilentMoonEndPoints/SilentMoonEndPoint.swift
+++ Sources/SilentMoonData/SilentMoonEndPoints/SilentMoonEndPoint.swift
@@ -20,9 +20,9 @@ public enum SilentMoonEndPoint: EndPoint {
     case googleLogin(idToken: String)
     case forgotPassword(email: String)
     case resetPassword(email: String, otp: String, newPassword: String)
-    case updateAccount(name : String , lastname : String , avatar: String)
-//    case getAccountHome
-//    case getUserProfile(id: Int)
+    //case updateAccount(name : String , lastname : String , avatar: String)
+    //case getAccountHome
+    //case getUserProfile(id: Int)
     case getCourses(page: Int, limit: Int)
     case getCourseDetail(id: Int)
     
@@ -70,12 +70,6 @@ public enum SilentMoonEndPoint: EndPoint {
             return "courses/"
         case .getCourseDetail(let id):
             return "courses/\(id)"
-        case .updateAccount(
-            name: let name,
-            lastname: let lastname,
-            avatar: let avatar
-        ):
-            <#code#>
         }
     }
 
@@ -91,12 +85,6 @@ public enum SilentMoonEndPoint: EndPoint {
             return .patch
         case .deleteReminder:
             return .delete
-        case .updateAccount(
-            name: let name,
-            lastname: let lastname,
-            avatar: let avatar
-        ):
-            <#code#>
         }
     }
 
@@ -155,12 +143,6 @@ public enum SilentMoonEndPoint: EndPoint {
             return .dictionary(["time": time, "days": days, "message": message])
         case .search, .getTopics, .getReminders, .deleteReminder, .getCourses, .getCourseDetail:
             return nil
-        case .updateAccount(
-            name: let name,
-            lastname: let lastname,
-            avatar: let avatar
-        ):
-            <#code#>
         }
     }
 
